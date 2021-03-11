b0866cbe75518964996d9c98a7d5e4a2515e73f8
JIALIN-HUANG <78190123+JIALIN-HUANG@users.noreply.github.com>
2021-03-11 12:38:36 -0800

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+//define our first cariable , the chacha
+let chacha = document.querySelector(".chacha");
+
+//result of the chacha's letter
+function turnChaL() {
+    document.body.style.background = "pink";
+    document.body.style.color = "black";
+}
+
+//add an event listner
+//when someone clicks on the chacha's image change the logo to chacha's letter
+
+chacha.addEventListener("click",turnChaL)
