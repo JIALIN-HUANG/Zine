e15fd60121c0958f3d19007b2ca69a38491d3f95
JIALIN-HUANG <78190123+JIALIN-HUANG@users.noreply.github.com>
2021-03-11 14:09:20 -0800

    java not working

--- script.js
+++ script.js
@@ -5,14 +5,40 @@ function play() {
 
 //define our first cariable , the chacha
 let chacha = document.querySelector(".chacha");
+let chaL = document.querySelector(".letter");
+
+//logo
+let logoD = document.querySelector(".logo");
 
 //result of the chacha's letter
 function turnChaL() {
     document.body.style.background = "pink";
-    document.body.style.color = "black";
+    chaL.classList.toggle("active")
+    logoD.style.display = "none";
 }
 
 //add an event listner
 //when someone clicks on the chacha's image change the logo to chacha's letter
 
-chacha.addEventListener("click",turnChaL)
+chacha.addEventListener("click",turnChaL);
+
+//----------  moli's letter  ----------------
+let moli = document.querySelector(".moli");
+let moliL = document.querySelector(".letter");
+
+//logo
+let logoD = document.querySelector(".logo");
+
+//result of the moli's letter
+function turnmoliL() {
+    document.body.style.background = "white";
+    moliL.classList.toggle("active")
+    logoD.style.display = "none";
+}
+
+//add an event listner
+//when someone clicks on the chacha's image change the logo to moli's letter
+
+moli.addEventListener("click",turnmoliL);
+
+//--------End--------------
